0d61caff90228bdc14b5af58ae95879ab23e7f6c
Ahendall <adham1.eldalil@gmail.com>
2024-10-11 18:29:51 +0400

    Completed Grayscale Filter

--- helpers.cpp
+++ helpers.cpp
@@ -2,7 +2,19 @@
 
 // Convert image to grayscale
 void Grayscale(int height, int width, vector<vector<RGBTRIPLE>> &image) {
-	return;
+	// Formula: Average of RGB values
+    // Time complexity: O(N) where N = m * n
+    // Space complexity: O(1)
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            float avg = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            image[i][j].rgbtRed = (BYTE) avg;
+            image[i][j].rgbtGreen = (BYTE) avg;
+            image[i][j].rgbtBlue = (BYTE) avg;
+        }
+    }
+    
+    return;
 }
 
 // Reflect image horizontally
